3517559cae3e6d86fae722bf88f162541078ec08
Prabhu-Manchineella_bcgovent <prabhu.manchineella@gov.bc.ca>
2023-02-24 15:11:26 +0000

    Commit message

--- while.py
+++ while.py
@@ -1,14 +1,9 @@
-import subprocess
-from git import Repo
 import os
+from git import Repo
 
-def push_to_github(repo_path, commit_message, access_token, new_branch_name):
+def push_to_github(repo_path, commit_message, access_token, branch_name):
     # Create a GitPython Repo object for the specified repository path
     repo = Repo(repo_path)
-
-    # Create a new branch and switch to it
-    repo.create_head(new_branch_name)
-    repo.heads[new_branch_name].checkout()
 
     # Add all changes to the staging area
     repo.index.add("*")
@@ -16,18 +11,23 @@ def push_to_github(repo_path, commit_message, access_token, new_branch_name):
     # Create a new commit with the specified commit message
     repo.index.commit(commit_message)
 
+    # Switch to the specified branch (or create it if it doesn't exist)
+    try:
+        repo.heads[branch_name].checkout()
+    except IndexError:
+        repo.create_head(branch_name)
+        repo.heads[branch_name].checkout()
+
     # Push changes to the remote repository using the provided personal access token
     remote_origin = repo.remote(name="origin")
     remote_url = remote_origin.url
     remote_url_with_token = remote_url.replace("https://", f"https://{access_token}@")
     remote_origin.set_url(remote_url_with_token)
-    remote_origin.push(refspec=f"refs/heads/{new_branch_name}")
+    remote_origin.push(refspec=f"refs/heads/{branch_name}")
 
 
 repo_path = "."
 commit_message = "Commit message"
 access_token = os.getenv("token")
-new_branch_name = "new-branch-name"
-
-push_to_github(repo_path, commit_message, access_token, new_branch_name)
-
+branch_name = "modify-script"
+push_to_github(repo_path, commit_message, access_token, branch_name)

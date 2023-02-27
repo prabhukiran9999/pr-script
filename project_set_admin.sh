04825e305004022a5b98d4b747c2b9714e53b375
Prabhu-Manchineella_bcgovent <prabhu.manchineella@gov.bc.ca>
2023-02-27 16:37:48 +0000

    remove projects

--- project_set_admin.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-license_plate=$(cat <(LC_ALL=C tr -dc a-z </dev/urandom | head -c 1) <(LC_ALL=C tr -dc a-z0-9 </dev/urandom | head -c 5) <(echo))
-mkdir -p projects/$license_plate/accounts
-touch projects/$license_plate/accounts/terragrunt.hcl
-echo "this is a line" > projects/$license_plate/accounts/terragrunt.hcl
-echo "project set created"
